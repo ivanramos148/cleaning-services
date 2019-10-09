926e8e2ad224f984ca3355f31435be413485c106
ivan <ivanramo148248s@gmail.com>
2019-10-09 15:32:45 -0700

    first initial

---
+++ src/main.js
@@ -0,0 +1,8 @@
+import './styles.css';
+import $ from 'jquery';
+import 'bootstrap/dist/css/bootstrap.min.css';
+//import { **insert prototype name** } from './cleaningService.js';
+
+$(document).ready(function() {
+
+});
